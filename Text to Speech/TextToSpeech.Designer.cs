30d8db4be0d0b6290f8fb323a55ab1967b95ca6e
Ahsan Javed <iahsanjaved@hotmail.com>
2018-03-13 19:45:56 +0500

    Uploaded source code

---
+++ Text to Speech/TextToSpeech.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Text_to_Speech
+{
+    partial class TextToSpeech
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.speechButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(335, 244);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "";
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // speechButton
+            // 
+            this.speechButton.Location = new System.Drawing.Point(142, 269);
+            this.speechButton.Name = "speechButton";
+            this.speechButton.Size = new System.Drawing.Size(75, 23);
+            this.speechButton.TabIndex = 1;
+            this.speechButton.Text = "Speech";
+            this.speechButton.UseVisualStyleBackColor = true;
+            this.speechButton.Click += new System.EventHandler(this.speechButton_Click);
+            // 
+            // TextToSpeech
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 307);
+            this.Controls.Add(this.speechButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "TextToSpeech";
+            this.Text = "Text to Speech";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.Button speechButton;
+    }
+}
+
---
+++ Text to Speech/TextToSpeech.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Text_to_Speech
+{
+    public partial class TextToSpeech : Form
+    {
+        SpeechSynthesizer synthesizer;
+        public TextToSpeech()
+        {
+            InitializeComponent();
+
+            synthesizer = new SpeechSynthesizer();
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void speechButton_Click(object sender, EventArgs e)
+        {
+            // Dispose the synthesizer object
+            synthesizer.Dispose();
+
+            if (inputTextBox.Text != "")
+            {
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(inputTextBox.Text);
+            }
+        }
+
+      
+    }
+}
